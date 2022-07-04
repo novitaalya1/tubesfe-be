a7c7d76ba06f63db3e9044f5971dda3bb602bcef
secondline16 <ahmadizulfan6@gmail.com>
2022-07-04 21:35:13 +0700

    delete.php

--- delete.php
+++ delete.php
@@ -1,2 +1,10 @@
 <?php
+    $url = "https://y9d9l5wr.directus.app/items/biodata?fields=nama,tgl_lahir,tinggi,berat,kota.nama,agama.nama";
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($curl);
+    $hasil = json_decode($response, true);
+
+    callAPI('DELETE', 'https://github.com/novitaalya1/tubesfe-be.git' . $id, false);
 ?>
