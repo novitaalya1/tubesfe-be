24932558ad7bdd2bdfbe84706ee57ee4bc24de12
Muhammad Sultan Rafi <msultanrafi@gmail.com>
2022-07-04 23:08:49 +0700

    Szultan

--- create.php
+++ create.php
@@ -1,12 +1,15 @@
+<?php
 $data_array =  array(
-      "customer"        => $user['User']['customer_id'],
-      "payment"         => array(
-            "number"         => $this->request->data['account'],
-            "routing"        => $this->request->data['routing'],
-            "method"         => $this->request->data['method']
+        "customer"        => $user['User']['customer_id'],
+        "payment"         => array(
+        "number"         => $this->request->data['account'],
+        "routing"        => $this->request->data['routing'],
+        "method"         => $this->request->data['method']
       ),
 );
-$make_call = callAPI('POST', 'https://api.example.com/post_url/', json_encode($data_array));
+$make_call = callAPI('POST', 'https://y9d9l5wr.directus.app/items/biodata', json_encode($data_array));
 $response = json_decode($make_call, true);
 $errors   = $response['response']['errors'];
 $data     = $response['response']['data'][0];
+
+?>
--- delete.php
+++ delete.php
@@ -6,5 +6,9 @@
     $response = curl_exec($curl);
     $hasil = json_decode($response, true);
 
+<<<<<<< Updated upstream
     callAPI('DELETE', 'https://y9d9l5wr.directus.app/items/biodata?fields=nama,tgl_lahir,tinggi,berat,kota.nama,agama.nama' . $id, false);
+=======
+    callAPI('DELETE', $$url . $id, false);
+>>>>>>> Stashed changes
 ?>
