e5f8531cfd388b27ef3892d210f6e16ee827c518
Dhimaas <108680082+Dhimaas@users.noreply.github.com>
2022-07-05 16:41:32 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,5 +1,17 @@
 <?php
-    dhim kerjain dhim
-
-
+    if(isset($_POST['update']))
+	{
+		$id = $_POST['id'];
+		
+		$query = "UPDATE 'mahasiswa' SET nama'$_POST[nama]',tgl_lahir'$_POST[tgl_lahir]',kota'$_POST[kota]',agama'$_POST[agama]',tinggi'$_POST[tinggi]',berat'$_POST[berat]' where id='$_POST[id]'"
+		$query_run = mysqli_query($connection,$query);
+	}
+	if($query_run)
+	{
+		echo '<script type="text/javascript"> alert("Data Edited") </script>
+	}
+	else
+	{
+		echo '<script type="text/javascript"> alert("Data Not Edited")
+	}
 ?>
